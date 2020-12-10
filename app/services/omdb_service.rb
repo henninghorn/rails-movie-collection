099980bb0a499e5bec35025f1a9fb531825d7e27
Henning Horn <hh@henninghorn.dk>
2020-12-10 23:28:57 +0100

    Add exception

--- app/services/omdb_service.rb
+++ app/services/omdb_service.rb
@@ -3,6 +3,9 @@ require 'omdb/api'
 class OmdbService
     def initialize()
         api_key = ENV["OMDB_API_KEY"]
+
+        raise "Missing Omdb api key. Remember to add OMDB_API_KEY to config/application.yml" if api_key.nil?
+
         @client = Omdb::Api::Client.new(api_key: api_key)
     end
 
